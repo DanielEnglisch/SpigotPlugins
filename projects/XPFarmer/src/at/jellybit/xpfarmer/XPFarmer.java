3ba4be939a255afc19287b3920ddd208f7cd5d33
Daniel Englisch <xeroaustria@hotmail.com>
2017-12-20 17:37:02 +0100

    added xp drops to sugar canes

--- projects/XPFarmer/src/at/jellybit/xpfarmer/XPFarmer.java
+++ projects/XPFarmer/src/at/jellybit/xpfarmer/XPFarmer.java
@@ -1,5 +1,7 @@
 package at.jellybit.xpfarmer;
 
+import java.util.Random;
+
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
@@ -9,7 +11,10 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.minecraft.server.v1_12_R1.EntityExperienceOrb;
@@ -30,17 +35,52 @@ public class XPFarmer extends JavaPlugin implements Listener {
 		super.onDisable();
 	}
 
+	@EventHandler
+	public void onSugarPlace(BlockPlaceEvent e) {
+		e.getBlock().setMetadata("PLACED", new FixedMetadataValue(this, "true"));
+	}
+
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent e) {
 
-		if (e.getBlock().getData() == 0x7) {
-			spawnOrb(e.getBlock().getLocation(), 3, 1);
+		if (e.getBlock().getType() == Material.SUGAR_CANE_BLOCK) {
+			int hasTop = 1;
+			if (e.getBlock().getRelative(0, 1, 0).getType() == Material.SUGAR_CANE_BLOCK) {
+				e.getBlock().getRelative(0, 1, 0).setType(Material.AIR);
+				hasTop = 2;
+
+			}
+
+			e.getBlock().setType(Material.AIR);
+
+			if (e.getPlayer().getInventory().firstEmpty() == -1)
+				e.getPlayer().getWorld().dropItem(e.getBlock().getLocation(),
+						new ItemStack(Material.SUGAR_CANE, 1 * hasTop));
+			else
+				e.getPlayer().getInventory().addItem(new ItemStack(Material.SUGAR_CANE, 1 * hasTop));
+
+			if (!e.getBlock().hasMetadata("PLACED")) {
+				spawnOrb(e.getBlock().getLocation(), 1 * hasTop, 1);
+
+			}
+
+		}
+
+		if (e.getBlock().getType() == Material.WHEAT || e.getBlock().getType() == Material.CARROT
+				|| e.getBlock().getType() == Material.POTATO) {
+			if (e.getBlock().getData() == 0x7) {
+				spawnOrb(e.getBlock().getLocation(), 1, 1);
+			}
 		}
 
 	}
 
 	public void spawnOrb(org.bukkit.Location l, int amount, int value) {
+
+		if (new Random().nextBoolean())
+			return;
+
 		double x = l.getX(), y = l.getY(), z = l.getZ();
 		World w = ((CraftWorld) l.getWorld()).getHandle();
 		for (int i = 0; i < value; i++) {
